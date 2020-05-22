376f36610093f5982462b7d6e022f50c76998b32
silvandrici <silvandrici@gmail.com>
2020-05-22 23:10:29 +0200

    work

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -15,7 +15,7 @@
  */
 package io.fabric8.quickstarts.camel;
 
-import org.apache.camel.builder.RouteBuilder;
+// import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -25,17 +25,17 @@ import org.springframework.context.annotation.ImportResource;
  */
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application extends RouteBuilder {
+public class Application /* extends RouteBuilder */ {
 
     // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
+    /* @Override
     public void configure() throws Exception {
         from("timer://foo?period=5000")
             .setBody().constant("Hello World")
             .log(">>> ${body}");
-    }
+    } */
 }
