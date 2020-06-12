18a5a89b5408f0c4a1cc2911d8241cb2f9cf870e
Ubuntu <silvandrici@gmail.com>
2020-06-12 08:24:51 -0700

    classpath test

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -20,9 +20,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
  */
+@RestController
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
 public class Application /* extends RouteBuilder */ {
@@ -31,4 +37,9 @@ public class Application /* extends RouteBuilder */ {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "greeting " + name;
+	}
 }
